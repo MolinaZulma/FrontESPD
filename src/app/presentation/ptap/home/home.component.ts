8b1aa519963864bdbbdb8e31887789c16ba5ef8d
Esteban Ruiz <rubianruiz05@my.com>
2023-11-18 23:35:52 -0500

    feat: added styles for component home at module ptap

--- src/app/presentation/ptap/home/home.component.ts
+++ src/app/presentation/ptap/home/home.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  constructor(private readonly _router: Router) {}
 
+  public ngOnInit(): void {}
+
+  public waterControll(): void {
+    this._router.navigate(['ptap', 'water-control']);
+  }
+  public activityRegister(): void {
+    this._router.navigate(['ptap', 'activity-register']);
+  }
+  public jardFormat(): void {
+    this._router.navigate(['ptap', 'jard-format']);
+  }
+  public ptapFormat(): void {
+    this._router.navigate(['ptap', 'ptap-format']);
+  }
 }
