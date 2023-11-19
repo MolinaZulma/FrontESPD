75f1d46f69ccfce9abb84d7159553ecac04fd4bb
Esteban Ruiz <rubianruiz05@my.com>
2023-11-18 23:31:06 -0500

    added components for ptap

--- src/app/presentation/ptap/ptap-routing.module.ts
+++ src/app/presentation/ptap/ptap-routing.module.ts
@@ -1,11 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PtapComponent } from './ptap.component';
+import { HomeComponent } from './home/home.component';
+import { ActivityRegisterComponent } from './activity-register/activity-register.component';
+import { PtapFormatComponent } from './ptap-format/ptap-format.component';
+import { WaterControlComponent } from './water-control/water-control.component';
+import { JardFormatComponent } from './jard-format/jard-format.component';
 
-const routes: Routes = [{ path: '', component: PtapComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: PtapComponent,
+    children: [
+      { path: 'home', component: HomeComponent },
+      { path: 'activity-register', component: ActivityRegisterComponent },
+      { path: 'ptap-format', component: PtapFormatComponent },
+      { path: 'water-control', component: WaterControlComponent },
+      { path: 'jard-format', component: JardFormatComponent },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class PtapRoutingModule { }
+export class PtapRoutingModule {}
--- src/app/presentation/ptap/ptap.component.ts
+++ src/app/presentation/ptap/ptap.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-ptap',
   templateUrl: './ptap.component.html',
   styleUrls: ['./ptap.component.css']
 })
-export class PtapComponent {
+export class PtapComponent implements OnInit {
+  constructor(private readonly _router: Router) {}
 
+  public ngOnInit(): void {   
+    this._router.navigate(['ptap', 'home']);
+  }
 }
